1269c54dea21920e36135089d1b447c75eb57f2b
Erica <echoi@alumni.ubc.ca>
2022-03-08 23:58:46 -0800

    day 1 challenge

---
+++ day_1/helloworld.py
@@ -0,0 +1,33 @@
+import math
+
+# operations
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 % 4)
+print(3 / 4)
+print(3 ** 4)
+print(3 // 4)
+
+# strings
+print('Erica')
+print('Choi')
+print('Canada')
+print('I am enjoying 30 days of python')
+
+# types
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4 - 4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+
+# data types
+print(type(('hello', 'world')))
+print(type({'hello', 'hello'}))
+print(type({'first_name': 'Erica'}))
+
+# euclidian distance
+print(math.sqrt((2 - 10)**2 + (3 - 8)**2))
+
+
